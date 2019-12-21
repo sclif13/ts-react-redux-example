12c0a4dc6ef43c775caf604638fc35600f0ed0c3
Alexandr Opryshko <sclif13@gmail.com>
2019-12-21 20:06:12 +0500

    Updates

---
+++ precache-manifest.a1ccb02a0588c8fb3836795a057e93e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcaf5ce171766bd70532d611d1793fe5",
+    "url": "/ts-react-redux-example/index.html"
+  },
+  {
+    "revision": "37dddb16073ea25db913",
+    "url": "/ts-react-redux-example/static/css/main.aacb0140.chunk.css"
+  },
+  {
+    "revision": "6b74d228fa5d877a5899",
+    "url": "/ts-react-redux-example/static/js/2.67fc70cc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ts-react-redux-example/static/js/2.67fc70cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37dddb16073ea25db913",
+    "url": "/ts-react-redux-example/static/js/main.eb2eb1e9.chunk.js"
+  },
+  {
+    "revision": "a7e3643e9f94d7a13eea",
+    "url": "/ts-react-redux-example/static/js/runtime-main.79d53ff6.js"
+  }
+]);
